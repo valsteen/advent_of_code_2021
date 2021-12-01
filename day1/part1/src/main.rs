d2b0fb2afd005372bac11109ec03385b6aa6baf5
Vincent Alsteen <vincent.alsteen@gmail.com>
2021-12-01 18:44:03 +0100

    reformat

--- day1/part1/src/main.rs
+++ day1/part1/src/main.rs
@@ -1,5 +1,4 @@
 fn main() {
-
     let input = "199
 200
 208
@@ -10,16 +9,19 @@ fn main() {
 269
 260
 263";
-    let measurements = input.split("\n").map(|x| x.parse::<i32>().unwrap());
+    let measurements = input.split('\n').map(|x| x.parse::<i32>().unwrap());
 
-    let increases = measurements.scan(None, |previous: &mut Option<i32>, current| {
-        let is_increase = match previous {
-            None => false,
-            Some(value) => current > *value
-        };
-        *previous = Some(current);
-        Some(is_increase)
-    }).filter(|is_increase| *is_increase).count();
+    let increases = measurements
+        .scan(None, |previous: &mut Option<i32>, current| {
+            let is_increase = match previous {
+                None => false,
+                Some(value) => current > *value,
+            };
+            *previous = Some(current);
+            Some(is_increase)
+        })
+        .filter(|is_increase| *is_increase)
+        .count();
 
     println!("{}", increases);
 }
--- day1/part2/src/main.rs
+++ day1/part2/src/main.rs
@@ -1,7 +1,6 @@
 use itertools::Itertools;
 
 fn main() {
-
     let input = "199
 200
 208
@@ -12,19 +11,24 @@ fn main() {
 269
 260
 263";
-    let measurements : Vec<i32> = input.split("\n").map(|x| x.parse::<i32>().unwrap()).collect_vec();
+    let measurements: Vec<i32> = input
+        .split('\n')
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect_vec();
     let windows = measurements.windows(3);
 
-
-    let increases = windows.scan(None, |previous: &mut Option<i32>, current| {
-        let sum = current.iter().sum();
-        let is_increase = match previous {
-            None => false,
-            Some(value) => sum > *value
-        };
-        *previous = Some(sum);
-        Some(is_increase)
-    }).filter(|is_increase| *is_increase).count();
+    let increases = windows
+        .scan(None, |previous: &mut Option<i32>, current| {
+            let sum = current.iter().sum();
+            let is_increase = match previous {
+                None => false,
+                Some(value) => sum > *value,
+            };
+            *previous = Some(sum);
+            Some(is_increase)
+        })
+        .filter(|e| *e)
+        .count();
 
     println!("{}", increases);
 }
