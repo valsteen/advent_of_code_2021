f9e9899042e0f213076ae9e58d299dbf943b8de2
Vincent Alsteen <vincent.alsteen@gmail.com>
2021-12-04 13:15:06 +0100

    day4 part1

--- day4/day4_part1/src/main.rs
+++ day4/day4_part1/src/main.rs
@@ -1,6 +1,57 @@
 use itertools::Itertools;
+use std::collections::HashSet;
 use std::io::BufRead;
 use std::{io, mem};
+
+struct Grid {
+    marks: HashSet<(usize, usize)>,
+    rows: Vec<Vec<u8>>,
+    winner: bool,
+}
+
+impl Grid {
+    fn new(rows: Vec<Vec<u8>>) -> Self {
+        Self {
+            marks: HashSet::new(),
+            rows,
+            winner: false,
+        }
+    }
+
+    fn winning_column(&self, x: usize) -> bool {
+        (0..self.rows.len()).all(|y| self.marks.contains(&(x, y)))
+    }
+
+    fn winning_row(&self, y: usize) -> bool {
+        (0..self.rows.get(y).unwrap().len()).all(|x| self.marks.contains(&(x, y)))
+    }
+
+    fn mark(&mut self, number: u8) {
+        for (y, row) in self.rows.iter().enumerate() {
+            for x in row
+                .iter()
+                .enumerate()
+                .filter_map(|(x, value)| (*value == number).then(|| x))
+            {
+                self.marks.insert((x, y));
+                self.winner = self.winner || self.winning_column(x) || self.winning_row(y);
+            }
+        }
+    }
+
+    fn score(&self) -> usize {
+        self.rows
+            .iter()
+            .enumerate()
+            .map(|(y, row)| {
+                row.iter().enumerate().filter_map(move |(x, value)| {
+                    (!self.marks.contains(&(x, y))).then(|| *value as usize)
+                })
+            })
+            .flatten()
+            .sum()
+    }
+}
 
 fn main() {
     let stdin = io::stdin();
@@ -16,7 +67,7 @@ fn main() {
 
     let lines = lines.skip(1).chain(vec!["".to_string()]);
 
-    let grids: Vec<Vec<Vec<u8>>> = lines
+    let mut grids: Vec<Grid> = lines
         .scan(vec![], |grid, line| {
             let numbers = line
                 .split_whitespace()
@@ -24,7 +75,7 @@ fn main() {
                 .flatten()
                 .collect_vec();
             if numbers.is_empty() {
-                Some(Some(mem::take(grid)))
+                Some(Some(Grid::new(mem::take(grid))))
             } else {
                 grid.push(numbers);
                 Some(None)
@@ -33,9 +84,18 @@ fn main() {
         .flatten()
         .collect();
 
-    for grid in grids {
-        println!("{:?}", grid)
+    'main: for draw in draws {
+        for (n, grid) in grids.iter_mut().enumerate() {
+            grid.mark(draw);
+            if grid.winner {
+                println!(
+                    "{} wins: {} ; {}",
+                    n,
+                    grid.score(),
+                    draw as usize * grid.score()
+                );
+                break 'main;
+            }
+        }
     }
-
-    println!("{:?}", draws);
 }
