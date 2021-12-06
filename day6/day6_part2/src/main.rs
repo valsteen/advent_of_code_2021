5a035b30c3e67282858a861e0aa89d1e20320804
Vincent Alsteen <vincent.alsteen@gmail.com>
2021-12-06 09:13:17 +0100

    day6 part2

---
+++ day6/day6_part2/src/main.rs
@@ -0,0 +1,33 @@
+use std::collections::HashMap;
+use std::io;
+use std::io::BufRead;
+
+fn main() {
+    let stdin = io::stdin();
+    let line = stdin.lock().lines().flatten().next().unwrap();
+    let ages: Vec<usize> = line.split(',').map(str::parse).flatten().collect();
+
+    let mut counts_per_age: HashMap<usize, usize> = HashMap::new();
+    for age in ages {
+        *counts_per_age.entry(age).or_default() += 1;
+    }
+
+    for _ in 0..256 {
+        let mut next = HashMap::new();
+
+        for (age, count) in counts_per_age.into_iter() {
+            match age {
+                0 => {
+                    *next.entry(6).or_default() += count;
+                    next.insert(8, count);
+                }
+                _ => {
+                    *next.entry(age - 1).or_default() += count;
+                }
+            }
+        }
+        counts_per_age = next;
+    }
+
+    println!("{}", counts_per_age.values().sum::<usize>());
+}
