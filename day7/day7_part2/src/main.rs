78c1dc1dfc278e939a5de518aa7ae152b476d8c0
Vincent Alsteen <vincent.alsteen@gmail.com>
2021-12-07 09:52:23 +0100

    type annotation not needed

--- day7/day7_part2/src/main.rs
+++ day7/day7_part2/src/main.rs
@@ -5,11 +5,8 @@ use std::io::Read;
 fn main() {
     let stdin = io::stdin();
     let positions = stdin.lock().bytes().flatten().map_into::<char>().group_by(|c| c.is_digit(10));
-    let positions = positions
-        .into_iter()
-        .map(|(_, v)| String::from_iter(v).parse::<i64>())
-        .flatten()
-        .collect_vec();
+    let positions =
+        positions.into_iter().map(|(_, v)| String::from_iter(v).parse()).flatten().collect_vec();
 
     if let MinMaxResult::MinMax(min, max) = positions.iter().minmax() {
         let (destination, fuel) = (*min..=*max)
