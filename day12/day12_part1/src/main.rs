d7aeb6e799203257770f79d146080e426403ad1b
Vincent Alsteen <vincent.alsteen@gmail.com>
2021-12-12 10:28:52 +0100

    day 12 part1

---
+++ day12/day12_part1/src/main.rs
@@ -0,0 +1,48 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+use std::io::{stdin, BufRead};
+
+fn visit(paths: &HashMap<String, Vec<String>>, visited: &[String], counter: &mut usize) {
+    let current = visited.last().unwrap();
+    if current.eq("end") {
+        *counter += 1;
+        return;
+    }
+    if let Some(destinations) = paths.get(current) {
+        for destination in destinations.iter().filter(|destination| {
+            !visited.iter().tuple_windows::<(_, _)>().contains(&(current, *destination))
+        }) {
+            if destination.as_str().ge("a") && visited.contains(destination) {
+                continue;
+            }
+            let mut visited = visited.to_vec();
+            visited.push(destination.clone());
+            visit(paths, &visited, counter)
+        }
+    }
+}
+
+fn main() {
+    let stdin = stdin();
+    let paths = stdin
+        .lock()
+        .lines()
+        .flatten()
+        .map(|x| {
+            x.split_once('-').map(|(source, destination)| {
+                vec![
+                    (source.to_string(), destination.to_string()),
+                    (destination.to_string(), source.to_string()),
+                ]
+            })
+        })
+        .flatten()
+        .flatten()
+        .sorted()
+        .into_group_map();
+
+    let mut counter = 0;
+    visit(&paths, &["start".to_string()], &mut counter);
+
+    println!("{}", counter)
+}
