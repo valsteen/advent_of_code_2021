1252e7244b4506970e35ad077dfa88a6d475df29
Vincent Alsteen <vincent.alsteen@gmail.com>
2021-12-12 13:09:52 +0100

    down to 54ms (-34%)

--- day12/day12_part2/src/main.rs
+++ day12/day12_part2/src/main.rs
@@ -2,26 +2,26 @@ use itertools::Itertools;
 use std::collections::HashMap;
 use std::io::{stdin, BufRead};
 
-fn visit<'a>(
-    paths: &'a HashMap<String, Vec<String>>,
-    visited: &mut Vec<&'a str>,
-    visited_small_cave: Option<&'a str>,
-    counter: &mut usize,
+fn visit(
+    paths: &HashMap<i32, Vec<i32>>,
+    visited: &mut Vec<i32>,
+    visited_small_cave: Option<i32>,
+    counter: &mut i32,
 ) {
     let &current = visited.last().unwrap();
-    if current.eq("end") {
+    if current == 0 {
         *counter += 1;
         return;
     }
-    if let Some(destinations) = paths.get(current) {
+    if let Some(destinations) = paths.get(&current) {
         for (visited_small_cave, destination) in destinations
             .iter()
-            .filter_map(|destination| {
-                if destination.as_str().lt("a") {
+            .filter_map(|&destination| {
+                if destination.is_positive() {
                     Some((visited_small_cave, destination))
                 } else {
                     let visit_count =
-                        visited.iter().filter(|&cave| cave.eq(destination)).take(2).count();
+                        visited.iter().filter(|&cave| cave.eq(&destination)).take(2).count();
                     match (visit_count, &visited_small_cave) {
                         (0, _) => Some((visited_small_cave, destination)),
                         (1, None) => Some((Some(destination), destination)),
@@ -32,14 +32,31 @@ fn visit<'a>(
             })
             .collect_vec()
         {
-            visited.push(destination.as_str());
+            visited.push(destination);
             visit(paths, visited, visited_small_cave, counter);
             visited.pop().unwrap();
         }
     }
 }
 
+fn make_id(s: &str) -> i32 {
+    if s.eq("end") {
+        return 0;
+    }
+    let small = s.ge("a");
+    let result = s
+        .bytes()
+        .map(|c| c.to_ascii_uppercase())
+        .fold(0, |acc: i32, x| acc * 16 + (x - b'A') as i32);
+    if small {
+        -result
+    } else {
+        result
+    }
+}
+
 fn main() {
+    let start: i32 = make_id("start");
     let stdin = stdin();
     let paths = stdin
         .lock()
@@ -47,20 +64,20 @@ fn main() {
         .flatten()
         .map(|x| {
             x.split_once('-').map(|(source, destination)| {
-                vec![
-                    (source.to_string(), destination.to_string()),
-                    (destination.to_string(), source.to_string()),
-                ]
+                let source = make_id(source);
+                let destination = make_id(destination);
+
+                vec![(source, destination), (destination, source)]
             })
         })
         .flatten()
         .flatten()
-        .filter(|(_, destination)| !destination.eq("start"))
+        .filter(|&(_, destination)| destination != start)
         .sorted()
         .into_group_map();
 
     let mut counter = 0;
-    visit(&paths, &mut vec!["start"], None, &mut counter);
+    visit(&paths, &mut vec![start], None, &mut counter);
 
     println!("{}", counter)
 }
