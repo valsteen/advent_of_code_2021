604efe64a637a34b0683298cf18a53eb08362464
Vincent Alsteen <vincent.alsteen@gmail.com>
2021-12-12 11:10:56 +0100

    day12 part 2

---
+++ day12/day12_part2/src/main.rs
@@ -0,0 +1,63 @@
+use itertools::Itertools;
+use std::collections::HashMap;
+use std::io::{stdin, BufRead};
+
+fn visit(
+    paths: &HashMap<String, Vec<String>>,
+    visited: &[String],
+    visited_small_cave: Option<String>,
+    counter: &mut usize,
+) {
+    let current = visited.last().unwrap();
+    if current.eq("end") {
+        *counter += 1;
+        return;
+    }
+    if let Some(destinations) = paths.get(current) {
+        for (visited_small_cave, destination) in destinations.iter().filter_map(|destination| {
+            if destination.as_str().lt("a") {
+                Some((visited_small_cave.clone(), destination))
+            } else {
+                let visit_count = visited.iter().filter(|&cave| cave.eq(destination)).count();
+                match visit_count {
+                    0 => Some((visited_small_cave.clone(), destination)),
+                    1 => match visited_small_cave {
+                        None => Some((Some(destination.clone()), destination)),
+                        Some(_) => None,
+                    },
+                    _ => None,
+                }
+            }
+        }) {
+            let mut visited = visited.to_vec();
+            visited.push(destination.clone());
+            visit(paths, &visited, visited_small_cave, counter)
+        }
+    }
+}
+
+fn main() {
+    let stdin = stdin();
+    let paths = stdin
+        .lock()
+        .lines()
+        .flatten()
+        .map(|x| {
+            x.split_once('-').map(|(source, destination)| {
+                vec![
+                    (source.to_string(), destination.to_string()),
+                    (destination.to_string(), source.to_string()),
+                ]
+            })
+        })
+        .flatten()
+        .flatten()
+        .filter(|(_, destination)| !destination.eq("start"))
+        .sorted()
+        .into_group_map();
+
+    let mut counter = 0;
+    visit(&paths, &["start".to_string()], None, &mut counter);
+
+    println!("{}", counter)
+}
