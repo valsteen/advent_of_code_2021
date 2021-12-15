8a1c423ad018aa514eead7fe5894dfb88981c92c
Vincent Alsteen <vincent.alsteen@gmail.com>
2021-12-15 19:06:06 +0100

    prepare full map

--- day15/day15_part2/src/main.rs
+++ day15/day15_part2/src/main.rs
@@ -21,40 +21,37 @@ fn neighbours(x: usize, y: usize, width: usize, height: usize, mut f: impl FnMut
     }
 }
 
-fn visit(map: &[Vec<u8>]) -> usize {
-    let height = map.len();
-    let width = map.get(0).unwrap().len();
-
+fn visit(map: &HashMap<(usize, usize), u8>, width: usize, height: usize) -> usize {
     let mut best = usize::MAX;
     let mut to_visit = HashSet::new();
-    let mut visited = HashMap::<(usize, usize), usize>::new();
-    visited.insert((0, 0), 0);
+    let mut scores = HashMap::<(usize, usize), usize>::new();
+    scores.insert((0, 0), 0);
     to_visit.insert((0, 0));
 
     while !to_visit.is_empty() {
         let &(start_x, start_y) = to_visit
             .iter()
-            .max_by_key(|&&(x, y)| (Reverse(x + y), *visited.entry((x, y)).or_insert(usize::MAX)))
+            .max_by_key(|&&(x, y)| {
+                (Reverse(x + y), Reverse(*scores.entry((x, y)).or_insert(usize::MAX)))
+            })
             .unwrap();
 
         to_visit.remove(&(start_x, start_y));
 
-        let &mut score = visited.entry((start_x, start_y)).or_insert(usize::MAX);
-        neighbours(start_x, start_y, width * 5, height * 5, |x, y| {
-            let row = map.get(y % height).unwrap();
-            let &risk = row.get(x % width).unwrap();
-            let risk = (risk - 1 + (x / width) as u8 + (y / height) as u8) % 9 + 1;
+        let &mut score = scores.entry((start_x, start_y)).or_insert(usize::MAX);
+        neighbours(start_x, start_y, width, height, |x, y| {
+            let &risk = map.get(&(x, y)).unwrap();
 
             let current = risk as usize + score;
-            let &mut previous_best = visited.entry((x, y)).or_insert(usize::MAX);
+            let &mut previous_best = scores.entry((x, y)).or_insert(usize::MAX);
 
             if previous_best <= current || best <= current {
                 return;
             }
 
-            visited.insert((x, y), current);
+            scores.insert((x, y), current);
 
-            if x == width * 5 - 1 && y == height * 5 - 1 {
+            if x == width - 1 && y == height - 1 {
                 best = best.min(current);
                 return;
             }
@@ -67,13 +64,39 @@ fn visit(map: &[Vec<u8>]) -> usize {
 
 fn main() {
     let stdin = io::stdin();
-    let risks: Vec<Vec<u8>> = stdin
+    let mut max_x = 0;
+    let mut max_y = 0;
+
+    let mut map: HashMap<(usize, usize), u8> = stdin
         .lock()
         .lines()
         .flatten()
-        .map(|line| line.bytes().map(|x| x - b'0').collect())
+        .enumerate()
+        .map(|(y, line)| {
+            line.bytes()
+                .enumerate()
+                .map(|(x, c)| {
+                    max_x = max_x.max(x);
+                    max_y = max_y.max(y);
+                    ((x, y), c - b'0')
+                })
+                .collect::<Vec<((usize, usize), u8)>>()
+        })
+        .flatten()
         .collect();
 
-    let best = visit(&risks);
+    let height = max_y + 1;
+    let width = max_x + 1;
+
+    for y in 0..height * 5 {
+        for x in 0..width * 5 {
+            let &risk = map.get(&(x % width, y % height)).unwrap();
+            let new_risk = (risk - 1 + (x / width) as u8 + (y / height) as u8) % 9 + 1;
+            map.insert((x, y), new_risk);
+        }
+    }
+
+    let best = visit(&map, width * 5, height * 5);
+
     println!("{}", best);
 }
