9b13cfba04e31c280f689d4b2a24174b791aa937
Vincent Alsteen <vincent.alsteen@gmail.com>
2021-12-17 08:39:17 +0100

    day17 part2 - I have no idea why it worked

---
+++ day17/day17_part2/src/main.rs
@@ -0,0 +1,112 @@
+use itertools::Itertools;
+use std::cmp::Ordering;
+use std::io;
+use std::io::Read;
+
+struct Target {
+    x_min: i32,
+    x_max: i32,
+    y_min: i32,
+    y_max: i32,
+}
+
+#[derive(Debug)]
+struct Position {
+    x: i32,
+    y: i32,
+    x_velocity: i32,
+    y_velocity: i32,
+}
+
+enum Reachability {
+    Unreachable,
+    Reachable,
+    Hit,
+}
+
+impl Position {
+    fn advance(&mut self) {
+        self.x += self.x_velocity;
+        self.y += self.y_velocity;
+
+        self.x_velocity += match self.x_velocity.cmp(&0) {
+            Ordering::Less => 1,
+            Ordering::Equal => 0,
+            Ordering::Greater => -1,
+        };
+        self.y_velocity -= 1;
+    }
+
+    fn reachability(&self, target: &Target) -> Reachability {
+        if (target.x_min..=target.x_max).contains(&self.x)
+            && (target.y_min..=target.y_max).contains(&self.y)
+        {
+            Reachability::Hit
+        } else if match self.x_velocity.cmp(&0) {
+            Ordering::Equal => {
+                if !(target.x_min..=target.x_max).contains(&self.x) {
+                    false
+                } else if self.y >= target.y_min {
+                    true
+                } else {
+                    self.y_velocity > 0
+                }
+            }
+            Ordering::Less => self.x >= target.x_min,
+            Ordering::Greater => self.x <= target.x_max,
+        } {
+            Reachability::Reachable
+        } else {
+            Reachability::Unreachable
+        }
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let numbers =
+        &stdin.lock().bytes().flatten().group_by(|&c| (b'0'..=b'9').contains(&c) || c == b'-');
+    let (x_min, x_max, y_min, y_max) = numbers
+        .into_iter()
+        .map(|(_, chunk)| String::from_iter(chunk.map(char::from)).parse())
+        .flatten()
+        .tuples()
+        .next()
+        .unwrap();
+
+    let target = Target { x_min, x_max, y_min, y_max };
+    let mut winners = 0;
+
+    for x_velocity in 0..x_max * 2 {
+        let mut y_velocity = y_min;
+
+        loop {
+            let mut position = Position { x: 0, y: 0, x_velocity, y_velocity };
+
+            loop {
+                match position.reachability(&target) {
+                    Reachability::Unreachable => break,
+                    Reachability::Reachable => {}
+                    Reachability::Hit => {
+                        winners += 1;
+                        break;
+                    }
+                }
+                position.advance();
+            }
+
+            if position.x < target.x_min {
+                break;
+            }
+
+            if y_velocity > 1000 {
+                // lame breaking condition but hey it works
+                break;
+            }
+
+            y_velocity += 1;
+        }
+    }
+
+    println!("{}", winners);
+}
