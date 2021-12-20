a59fa81d5b2da8ca654c77decbf7b2c8b81a0486
Vincent Alsteen <vincent.alsteen@gmail.com>
2021-12-20 12:33:29 +0100

    simplify

--- day20/day20_part1/src/main.rs
+++ day20/day20_part1/src/main.rs
@@ -1,55 +1,56 @@
 use itertools::Itertools;
+use rayon::prelude::*;
 use std::borrow::Borrow;
 use std::io;
 use std::io::BufRead;
 
 #[inline]
-fn get_at(input: &[Vec<bool>], x: i32, y: i32, dx: i32, dy: i32, default: usize) -> usize {
-    let x = x + dx;
-    let y = y + dy;
+fn get_at(input: &[Vec<u8>], x: i32, y: i32, default: u8) -> u8 {
     if x < 0 || y < 0 || y > input.len() as i32 - 1 || x > input[0].len() as i32 - 1 {
         default
-    } else if input[y as usize][x as usize] {
-        1
     } else {
-        0
+        input[y as usize][x as usize]
     }
 }
 
 #[inline]
-fn zone(input: &[Vec<bool>], x: i32, y: i32, default: usize) -> usize {
-    (-1..=1).map(|dy| (-1..=1).map(move |dx| get_at(input, x, y, dx, dy, default))).flatten().fold(0, |acc, bit| {
-        (acc << 1) + bit
-    })
+fn zone(input: &[Vec<u8>], x: i32, y: i32, default: u8) -> usize {
+    (-1..=1)
+        .map(|dy| (-1..=1).map(move |dx| get_at(input, x + dx, y + dy, default)))
+        .flatten()
+        .fold(0, |acc, bit| (acc << 1) + bit as usize)
 }
 
 #[inline]
-fn enhance(input: &[Vec<bool>], enhancement: &[bool], x: i32, y: i32, default: usize) -> bool {
+fn enhance(input: &[Vec<u8>], enhancement: &[u8], x: i32, y: i32, default: u8) -> u8 {
     enhancement[zone(input, x, y, default)]
 }
 
-fn next_default(default: usize, enhancement: &[bool]) -> usize {
-    let default = if default == 1 {
+fn next_default(default: u8, enhancement: &[u8]) -> u8 {
+    if default == 1 {
         enhancement[(1 << 9) - 1]
     } else {
         enhancement[0]
-    };
-    if default { 1 } else { 0 }
+    }
 }
 
-fn enhance_image(image: &[Vec<bool>], enhancement: &[bool], default: usize) -> Vec<Vec<bool>> {
-    (-1..=image[0].len() as i32).map(|y| (-1..=image.len() as i32).map(|x| {
-        enhance(image, enhancement, x, y, default)
-    }).collect()).collect()
+fn enhance_image(image: &[Vec<u8>], enhancement: &[u8], default: u8) -> Vec<Vec<u8>> {
+    (-1..=image[0].len() as i32)
+        .into_par_iter()
+        .map(|y| {
+            (-1..=image.len() as i32).map(|x| enhance(image, enhancement, x, y, default)).collect()
+        })
+        .collect()
 }
 
 #[allow(dead_code)]
-fn display(image: &[Vec<bool>]) -> String {
-    (0..image[0].len()).map(|y| (0..image.len()).map(|x| {
-        if image[y][x] { "#" } else { "." }
-    }).collect::<String>()).join("\n")
+fn display(image: &[Vec<u8>]) -> String {
+    (0..image[0].len())
+        .map(|y| {
+            (0..image.len()).map(|x| if image[y][x] == 1 { "#" } else { "." }).collect::<String>()
+        })
+        .join("\n")
 }
-
 
 fn main() {
     let stdin = io::stdin();
@@ -60,18 +61,25 @@ fn main() {
         if is_empty {
             None
         } else {
-            Some(lines.map(|line| line.chars().map(|c| c == '#').collect_vec()).collect_vec())
+            Some(
+                lines
+                    .map(|line| line.chars().map(|c| if c == '#' { 1 } else { 0 }).collect_vec())
+                    .collect_vec(),
+            )
         }
     });
 
     let enhancement = images.next().unwrap().into_iter().flatten().collect_vec();
     let image = images.next().unwrap();
 
-    let default = 0;
-    let new_image = enhance_image(&image, &enhancement, default);
+    let mut default = 0;
 
-    let default = next_default(default, &enhancement);
-    let new_image = enhance_image(&new_image, &enhancement, default);
-    let count = new_image.iter().flatten().filter(|&&b| b).count();
+    let new_image = (0..2).fold(image, |image, _| {
+        let new_image = enhance_image(&image, &enhancement, default);
+        default = next_default(default, &enhancement);
+        new_image
+    });
+
+    let count = new_image.par_iter().flatten().map(|&v| v as usize).sum::<usize>();
     println!("{}", count);
 }
